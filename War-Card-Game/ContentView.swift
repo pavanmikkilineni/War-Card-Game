102f55d196b84b7c6ed40c60d700c681d00eb2db
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2021-12-02 17:58:44 -0800

    Initial Commit

---
+++ War-Card-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War-Card-Game
+//
+//  Created by Pavan Mikkilineni on 27/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War-Card-Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War-Card-Game
+//
+//  Created by Pavan Mikkilineni on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
