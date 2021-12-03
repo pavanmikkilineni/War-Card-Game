70538ef943e557633526ad0cd095dac10d84c6ed
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2021-12-02 21:13:06 -0800

    SwiftUI War Card Game UI Construction

--- War-Card-Game/ContentView.swift
+++ War-Card-Game/ContentView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World")
-            .padding()
+        ZStack{
+            Image("background")
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding(.bottom,10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .padding(.bottom,10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
